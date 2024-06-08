5c0570e6a0a3039c3d5c5310f3fb580674a3f9df
Imalka <gunasekara.iusa@gmail.com>
2024-06-08 22:05:20 +0530

    test_test

--- tests/index.py
+++ tests/index.py
@@ -1,30 +1,4 @@
-import json
-import boto3
-from decimal import Decimal
-
-dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('cloud-resume-tf') # Use your table name here
-
-def lambda_handler(event, context):
-    try:
-        response = table.get_item(Key={'id': '0'})
-        if 'Item' not in response:
-            raise ValueError("Item with id '0' not found")
-
-        views = int(response['Item']['views'])  # Convert Decimal to int
-        views += 1
-
-        print(views)
-
-        table.put_item(Item={'id': '0', 'views': views})
-
-        return {
-            'statusCode': 200,
-            'body': {'views': views}
-        }
-
-    except Exception as e:
-        return {
-            'statusCode': 500,
-            'body': json.dumps({'errorMessage': str(e)})
-        }
+def lambda_handler():
+    x=4
+    return x
+    
--- tests/test_lambda_function.py
+++ tests/test_lambda_function.py
@@ -2,15 +2,10 @@ import pytest
 from index import lambda_handler
 
 def test_lambda_handler_error_handling():
-    # Simulate an error condition by providing a non-existent table name
-    event = {}
-    context = {}
+    
+    response = lambda_handler()
 
-    # Call the Lambda function
-    response = lambda_handler(event, context)
+    
+    assert response == 4
 
-    # Assert the status code
-    assert response['statusCode'] == 500
-
-    # Assert the presence of error message in the response body
-    assert 'errorMessage' in response['body']
+   
