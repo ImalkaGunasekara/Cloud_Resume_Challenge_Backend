73c22a0d2fd925154c7b420228f959b910ee62bb
Imalka <gunasekara.iusa@gmail.com>
2024-06-08 22:07:51 +0530

    test_test

--- tests/test_lambda_function.py
+++ tests/test_lambda_function.py
@@ -2,10 +2,15 @@ import pytest
 from index import lambda_handler
 
 def test_lambda_handler_error_handling():
-    
-    response = lambda_handler()
+    # Simulate an error condition by providing a non-existent table name
+    event = {}
+    context = {}
 
-    
-    assert response == 4
+    # Call the Lambda function
+    response = lambda_handler(event, context)
 
-   
+    # Assert the status code
+    assert response['statusCode'] == 500
+
+    # Assert the presence of error message in the response body
+    assert 'errorMessage' in response['body']
